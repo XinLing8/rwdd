6560ee985b61be29ea2120341a915130225f749a
XinLing8 <yapgenie8@gmail.com>
2025-05-09 21:44:40 +0800

    Create config.php

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+$con = new mysqli("localhost", "root", "", "quizzing_you")
+                                            //above is database name
+                                            //should be same with xampp
+or die ("Failed to connect to database".mysqli_connect_error());
+?>
